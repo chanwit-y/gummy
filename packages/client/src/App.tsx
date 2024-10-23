d73adba8a00a357e7f4ec673434772f0562aa9cb
cy <chanwit.yimneam@hotmail.com>
2024-10-23 17:42:45 +0700

    init bun 🎉🎉🎉

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,28 @@
+import { ReactFlow } from '@xyflow/react';
+// import {} from '@gummy/api';
+
+import '@xyflow/react/dist/style.css';
+import { API } from 'api';
+import { useEffect } from 'react';
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
+
+
+export default function App() {
+  useEffect(() => {
+    API()
+  }, []);
+
+  return (
+    <>
+      <div style={{ width: '100vw', height: '100vh' }}>
+        <ReactFlow nodes={initialNodes} edges={initialEdges} />
+      </div>
+    </>
+  );
+}
